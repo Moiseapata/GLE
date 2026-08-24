9a9e38727337cb0a657993e14777d94894d57f35
Moïse Apata <146359171+Moiseapata@users.noreply.github.com>
2026-08-24 13:58:58 +0200

    Update base path in Vite configuration
    Set base path to '/GLE/' to prevent blank page issue.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: './',
+  base: '/GLE/',  // <-- C'est LE réglage crucial pour éviter la page blanche !
   build: {
     outDir: 'dist/public',
   }
