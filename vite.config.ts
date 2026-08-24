69adf71c3f323da44fe6335ca3ddd2f2112ad0ba
Moïse Apata <146359171+Moiseapata@users.noreply.github.com>
2026-08-24 14:03:01 +0200

    Fix formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   root: './',
-  base: '/GLE/',  // <-- C'est LE réglage crucial pour éviter la page blanche !
+  base: '/GLE/',
   build: {
     outDir: 'dist/public',
   }
